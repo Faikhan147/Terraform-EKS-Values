c4c5e7794d14fa9a7f836b80d97bb2060d757019
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-28 12:40:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "region" {
   description = "AWS region name"
   type        = string
 }
+
+variable "vpc_id" {
+  type = string
+  description = "VPC ID where resources will be created"
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
