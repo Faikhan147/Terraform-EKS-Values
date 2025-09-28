15883f30209a1aa27e9914e47b859e555e3b9b6a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-28 06:53:33 +0530

    Create main.tf

---
+++ modules/security-group /main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "this" {
+  name        = "${var.env}-sg"
+  description = "${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-sg"
+  }
+}
