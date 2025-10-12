2cc3b4f3ab5192ee3dad65f990250056b2316d64
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-13 02:36:09 +0530

    Update backend.tf

--- environment/staging/backend.tf
+++ environment/staging/backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-all-envs"
     key            = "eks/values/staging/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "terraform-locks-values-staging"
   }
